b54d672eb74221542526e4d73e440b12079700f0
RyanzinhoDias <ryanddsn0@gmail.com>
2024-06-21 21:38:41 -0300

    Comentário no pacote arquivo

--- bibliotecas/arquivo/__init__.py
+++ bibliotecas/arquivo/__init__.py
@@ -1,3 +1,4 @@
+#função que tenta ler o arquivo, caso ele não exista acontece uma exceção que tem a instrução de criar um arquivo
 def criacao_do_arquivo():
     try:
         arquivo = open("sistema_menu.txt", "r")
@@ -6,8 +7,9 @@ def criacao_do_arquivo():
         print('Arquivo sistema_menu.txt criado com sucesso')
     finally:
         arquivo.close()
-
-
+    
+    
+#função de leitura do arquivo
 def ler_arquivo():
     with open("sistema_menu.txt", "r") as arquivo:
         texto = arquivo.readlines()
@@ -15,5 +17,4 @@ def ler_arquivo():
             dados = linha.split(';')
             dados[1] = dados[1].replace('\n', '')
             print(f'{dados[0]:<30}{dados[1]:>3} anos')
-
 
