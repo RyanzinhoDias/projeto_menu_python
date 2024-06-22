f562e2e93af0215dc7a047a3f4a9ee15ad9ad0ea
RyanzinhoDias <ryanddsn0@gmail.com>
2024-06-21 22:01:20 -0300

    Comentários no do sistema principal

--- sistema.py
+++ sistema.py
@@ -1,20 +1,35 @@
+#pacotes e funções importadas
 from bibliotecas import interface
 from bibliotecas import funcoes
 from bibliotecas import arquivo
 from time import sleep
 
+#chamando o arquivo criado
+
 arquivo.criacao_do_arquivo()
 
 arq = "sistema_menu.txt"
 
+#Loop para fazer o sistema ficar ativo até que o usuário deseja finaliza-lo
+
 while True:
+
+    #Lista das informações que existem no menu 
+
     interface.menu(['Ver cadastrados', 'Cadastrar novo', 'Sair do sistema'])
+
+    #Variável para escolher uma opção
+
     decisao = funcoes.leiaint('\033[33mSua escolha:\033[m ')
+
+    #verificação de inteiros diferentes das opções do menu
 
     while decisao < 1 or decisao > 3:
         print('\033[31mERRO: DIGITE UM INTEIRO VÁLIDO\033[m')
         decisao = funcoes.leiaint('\033[33mSua escolha:\033[m ')
 
+    #verificação da opção digitada
+    
     if decisao == 3:
         funcoes.sair()
         break
